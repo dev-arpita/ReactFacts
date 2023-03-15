6b4ba0fa45814e2bfa620959d91740bc582d0340
Arpita Roy <arpita.roy.workar@gmail.com>
2023-03-15 20:08:48 +0530

    React Practice

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 // Spend a minute memorizing the line below
 // Re-write the line of code as best you can from memory
+/* Surprise! You probably thought you could just forget
+the line of code you just learned! Nope, not on my watch!
+
+Try to write that 1-liner of React code again! This time,
+see if you can figure out how to render an <ul> with 2+ <li>s inside*/
+ReactDOM.render(
+    <ul><li>Thing 1</li><li>Thing 2</li></ul>,
+    document.getElementById("root")
+)
