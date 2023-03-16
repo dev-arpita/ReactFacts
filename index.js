e76aa601bb951f98cd8051362cf264972b4c544e
Arpita Roy <arpita.roy.workar@gmail.com>
2023-03-16 19:53:22 +0530

    Thought experiment: use .append() instead of ReactDOM.render()?

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 import React from "react"
-import ReactDOM from "react-dom/client"
+import ReactDOM from "react-dom"
 
-const navbar = (
-    <nav>
-        <h1>Bob's Bistro</h1>
-        <ul>
-            <li>Menu</li>
-            <li>About</li>
-            <li>Contact</li>
-        </ul>
-    </nav>
+/**
+Challenge: fix our code!
+
+Don't forget, you're not using CDNs anymore, so there's no
+global "ReactDOM" variable to use anymore.
+ */
+
+const page = (
+    <div>
+        <h1>My awesome website in React</h1>
+        <h3>Reasons I love React</h3>
+        <ol>
+            <li>It's composable</li>
+            <li>It's declarative</li>
+            <li>It's a hireable skill</li>
+            <li>It's actively maintained by skilled people</li>
+        </ol>
+    </div>
 )
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(navbar)
-
-// ReactDOM.render(navbar, document.getElementById("root"))
-// ReactDOM.createRoot(document.getElementById("root")).render(navbar)
+ReactDOM.render(page, document.getElementById("root"))
+// document.getElementById("root").append(JSON.stringify(page))
